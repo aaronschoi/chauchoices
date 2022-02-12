f3113791612b355a93a0a23036475fe7460db7cb
Aaron Choi <connect@aaronschoi.com>
2022-02-11 21:18:54 -0800

    update

---
+++ components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>CHAUY ICON</div>
+            <Link href="/choices"><a>CHOICES</a></Link>
+        </div>
+    )
+}
