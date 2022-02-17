66991ebc122b123737471b583a413719c4089acb
Aaron Choi <connect@aaronschoi.com>
2022-02-17 15:56:56 -0800

    temp

--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,18 @@
-const API_URL : string = process.env.MAIN_URL || "none";
+import { TPosts, TSlugs } from "../types/apiTypes";
 
-async function fetchAPI() {
-  const res = await fetch(API_URL);
+const URL: string = process.env.API_URL || "none";
+
+const fetchAPI = async (param: string) => {
+  const res = await fetch(`${URL}/${param}`);
   const json = await res.json();
 
-  //error handling
   if (json.errors) {
     console.log(json.errors);
-    throw new Error("Failed to fetch API");
+    throw new Error(`fetching ${param} failed to fetch`);
   }
   return json;
-}
+};
 
-export async function getAllPosts() {
-  const data = await fetchAPI();
-  return data;
-}
+export const getPosts = async () : Promise<TPosts> => fetchAPI("posts");
+
+export const getSlugs = async () : Promise<TSlugs> => fetchAPI("slugs");
--- types/apiTypes.ts
+++ types/apiTypes.ts
@@ -1,51 +1,60 @@
-type TRating = 1 | 2 | 3 | 4 | 5
+type TRating = 1 | 2 | 3 | 4 | 5;
 
-export interface IPost {
-    postID : number,
-    image: string,
-    title : string,
-    headline : string,
-    description : string,
-    address : string,
-    ratings : {
-        tables : TRating,
-        chairs : TRating,
-        lighting : TRating,
-        sound : TRating,
-        smell : number,
-        outlets : TRating,
-        bathroom : TRating
-    },
-    outlets : {
-        quantity : number 
-    },
-    food : {
-        availability : string
-    },
-    tables : {
-        size : {
-            height : number,
-            width : number,
-            shape : string
-        },
-        quality : string,
-        quantity : number
-    },
-    chairs : {
-        size : {
-            height : number,
-            width : number
-        },
-        quality : string,
-        quanity : number
-    },
-    ambience : {
-        quality : string
-    },
-    cost : {
-        item : string,
-        price : number
-    }
+export interface ISlug {
+  slug: string;
+  image: string;
+  title: string;
+  blurb: string;
 }
 
-export type TPosts = [IPost]
+export interface IPost {
+  postID: number;
+  image: string;
+  title: string;
+  headline: string;
+  description: string;
+  address: string;
+  ratings: {
+    tables: TRating;
+    chairs: TRating;
+    lighting: TRating;
+    sound: TRating;
+    smell: number;
+    outlets: TRating;
+    bathroom: TRating;
+  };
+  outlets: {
+    quantity: number;
+  };
+  food: {
+    availability: string;
+  };
+  tables: {
+    size: {
+      height: number;
+      width: number;
+      shape: string;
+    };
+    quality: string;
+    quantity: number;
+  };
+  chairs: {
+    size: {
+      height: number;
+      width: number;
+    };
+    quality: string;
+    quanity: number;
+  };
+  ambience: {
+    quality: string;
+  };
+  cost: {
+    item: string;
+    price: number;
+  };
+}
+
+export type TPosts = [IPost];
+
+export type TSlugs = [ISlug];
